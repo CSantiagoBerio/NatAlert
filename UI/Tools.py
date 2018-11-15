d21a79f8760b0e687c65a70156126a5890bc3180
alejandra <alejandra.casanova@upr.edu>
2018-11-14 23:12:13 -0400

    COMMENTED SOME TIPS FOR THE TOOL HELPER CLASS. Changed the order of the lexer methods.

---
+++ UI/Tools.py
@@ -0,0 +1,4 @@
+#Deberiamos crear metodos que manejen la dataa que el usuario envia sobre la catastrofe
+#UNo q diga la implicacion en base a la categoria que recibe
+#Otro podria ser que en base a la hubicacion del usuario y la ubicacion de la tormeta para desarollar estrategias de ataque
+#por ejemplo si esta muy cerca la tormenta deberia buscar un safe place, si esta lejos puede desarollar lo q quiere
--- natlex.py
+++ natlex.py
@@ -31,6 +31,21 @@ reserved_words_map = { }
 for r in reserved:
     reserved_words_map[r.lower()] = r
 
+@TOKEN(identifier)
+def t_IDENTIFIER(t):
+    t.type = reserved.get(t.value,'IDENTIFIER')              #This checks if the identifier is part of the reserved words
+    return t
+@TOKEN(declaration)
+def t_DECLARATION(t):
+    t.type = reserved.get(t.value,'DECLARATION')
+    return t
+
+
+@TOKEN(method)
+def t_METHOD(t):
+    return t
+
+
 @TOKEN(double)
 def t_DOUBLE(t):
     r'\d'
@@ -46,18 +61,7 @@ def t_NUMBER(t):
 def t_CHAR(t):
     return t
 
-@TOKEN(identifier)
-def t_IDENTIFIER(t):
-    t.type = reserved.get(t.value,'IDENTIFIER')              #This checks if the identifier is part of the reserved words
-    return t
 
-@TOKEN(declaration)
-def t_DECLARATION(t):
-    return t
-
-@TOKEN(method)
-def t_METHOD(t):
-    return t
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
@@ -116,11 +120,12 @@ def t_COMMENT(t):               #definition of a comment, it will not return any
 
 lexer = lex(debug=0)
 
-lexer.input('1258403484934038585320029483905808203842028-185308-2-428-')
-lexer.input('x=12')
-lexer.input('hvhj')
-lexer.input('23.5423545')
-
+#lexer.input('1258403484934038585320029483905808203842028-185308-2-428-')
+#lexer.input('x=12')
+#lexer.input('hvhj')
+#lexer.input('23.5423545')
+lexer.input('create Catastrophe(){'
+            'let maria = new Storm()}')
 while True:
     tok = lexer.token()
     if not tok:
