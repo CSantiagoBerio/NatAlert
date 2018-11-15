7b7710dba4a38e3ae552fff97af856f14fb77937
alejandra <alejandra.casanova@upr.edu>
2018-11-14 22:44:27 -0400

    mapp of reserved words, definition of methods

--- natlex.py
+++ natlex.py
@@ -1,30 +1,40 @@
-#from ply import lex
-#import ply.yacc as yacc
 from ply.lex import *
 
-reserved = {'if':'IF' ,'then':'THEN','then':'THEN','else':'ELSE','while':'WHILE','for':'FOR','catastrophe':'CATASTROPHE'}
+reserved = {'if':'IF' ,
+            'then':'THEN',
+            'then':'THEN',
+            'else':'ELSE',
+            'while':'WHILE',
+            'for':'FOR',
+            'catastrophe':'CATASTROPHE',
+            'create':'CREATE'}
 
-tokens = ['IDENTIFIER', 'NUMBER', 'CHAR', 'DECLARATION', 'METHOD', 'EQUAL', 'ENDLINE'] + reserved.values()
-literals = "+-/*}{[]()"                                                 #This are literal characters the lexer interprets them as they are
+tokens = ['IDENTIFIER', 'NUMBER', 'DOUBLE', 'CHAR', 'DECLARATION', 'METHOD', 'EQUAL', 'ENDLINE','COMMA'] + list(reserved.values())
+
+literals = "+-/*}{[]()"                                                                          #This are literal characters the lexer interprets them as they are
+
 digit = r'[0-9]'
 number = r'[0 | 1-9][0-9]*'
 character = r'[a-zA-Z]'
+double = r'[-+]?[0-9]+(\.([0-9]+)?([eE][-+]?[0-9]+)?|[eE][-+]?[0-9]+)'
 lefparen = r'('
 rigparen = r')'
 identifier = r'' + character + r'(' + character + r'|' + number + r')*'
 declaration = r'' + identifier + r'= (' + number + r'|' + character + r')* ;'
 method = identifier + r'[(]' + r'[)]'
-#catastrophe = r'[hurricane]'+character
-#category = r'[1-5]'
-#coordinates = r'' + lefparen + number + r'.'+number+r','+number+'.'+number+rigparen
+
 t_EQUAL = r'='
 t_ENDLINE = r';'
+t_COMMA = r','
 
-@TOKEN(identifier)
-def t_IDENTIFIER(t):
-    t.type = reserved.values(t.value,'IDENTIFIER') #This checks if the identifier is part of the reserved words
+reserved_words_map = { }
+for r in reserved:
+    reserved_words_map[r.lower()] = r
+
+@TOKEN(double)
+def t_DOUBLE(t):
+    r'\d'
     return t
-
 
 @TOKEN(number)
 def t_NUMBER(t):
@@ -32,25 +42,33 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
-
 @TOKEN(character)
 def t_CHAR(t):
+    return t
+
+@TOKEN(identifier)
+def t_IDENTIFIER(t):
+    t.type = reserved.get(t.value,'IDENTIFIER')              #This checks if the identifier is part of the reserved words
     return t
 
 @TOKEN(declaration)
 def t_DECLARATION(t):
     return t
 
+@TOKEN(method)
+def t_METHOD(t):
+    return t
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-#Aparece en el tutorial
 def t_newLine(t):
     r' \n+'
     t.lexer.lineno+=len(t.value)
-#****************definition of literals************************************************************
+
+#Literals
+
 def t_lbrace(t):
     r'\{'
     t.type='{'
@@ -91,20 +109,18 @@ def t_div(t):
     r'\/'
     t.type = '/'
     return t
-#**********************************************************************************************
+
 def t_COMMENT(t):               #definition of a comment, it will not return anything when it reads a #___
     r'\#.*'
     pass
 
-#*****************Debuging***************************************************************************
-
 lexer = lex(debug=0)
 
-lexer.input('12584034849340385853200294839058082038420280488350647352081-185308-2-428-')
+lexer.input('1258403484934038585320029483905808203842028-185308-2-428-')
 lexer.input('x=12')
+lexer.input('hvhj')
+lexer.input('23.5423545')
 
-#data = '3 + 4 * 10+ -20 *2'
-#lexer.input(data)
 while True:
     tok = lexer.token()
     if not tok:
